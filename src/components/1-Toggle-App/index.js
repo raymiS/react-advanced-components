3243563d326e3b8d56f08bd8931d8ef91d2742dd
Raymi Saldomando <raymi@able.co>
2018-03-02 16:44:42 -0500

    React Lesson Three: Compound Components

--- src/components/1-Toggle-App/index.js
+++ src/components/1-Toggle-App/index.js
@@ -1,5 +1,43 @@
 import React, { Component } from 'react';
 
+// Compound components allows you to share implicit state between the parent component and its children with some functionality as well. This gives the developer more control over the order of the component rendering. In this exercice we use three children that get pass some props and the state from the Toggle parent component. To pass the props, React uses a special maps method for that grabs the children that the developers defines, creates a copy of them and sets them up with the props the developers needs the children to have.
+
+const ToggleOn = ({ on, children }) => {
+  return on ? children : null;
+};
+
+const ToggleOff = ({ on, children }) => {
+  return on ? null : children;
+};
+
+const ToggleButton = ({ on, toggle, ...props }) => {
+  return <Switch on={on} onClick={toggle} {...props} />;
+};
+
+class Toggle extends Component {
+  static On = ToggleOn;
+  static Off = ToggleOff;
+  static Button = ToggleButton;
+  static defaultProps = { onToggle: () => {} };
+  state = { on: false };
+
+  toggle = () =>
+    this.setState(
+      ({ on }) => ({ on: !on }),
+      () => this.props.onToggle(this.state.on)
+    );
+
+  render() {
+    const children = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, {
+        on: this.state.on,
+        toggle: this.toggle
+      })
+    );
+    return <div>{children}</div>;
+  }
+}
+
 const Switch = ({ on, className = '', ...props }) => {
   return (
     <div className="toggle">
@@ -15,16 +53,14 @@ const Switch = ({ on, className = '', ...props }) => {
   );
 };
 
-class Toggle extends Component {
-  state = { on: false };
+const ToggleWrap = () => {
+  return (
+    <Toggle onToggle={on => console.log('toggle', on)}>
+      <Toggle.On>The button is on</Toggle.On>
+      <Toggle.Button />
+      <Toggle.Off>The button is off</Toggle.Off>
+    </Toggle>
+  );
+};
 
-  toggle = () => {
-    this.setState(state => ({ on: !this.state.on }));
-  };
-  render() {
-    const { on } = this.state;
-    return <Switch on={on} onClick={this.toggle} />;
-  }
-}
-
-export default Toggle;
+export default ToggleWrap;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './base.css';
 
-import Toggle from './components/1-Toggle-App';
+import ToggleWrap from './components/1-Toggle-App/';
 
-ReactDOM.render(<Toggle />, document.getElementById('root'));
+ReactDOM.render(<ToggleWrap />, document.getElementById('root'));
 registerServiceWorker();
