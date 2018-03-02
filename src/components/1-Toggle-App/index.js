58da28d7bb2f37ef218d462534614926fa9a943b
Raymi Saldomando <raymi@able.co>
2018-03-02 15:10:11 -0500

    React Lesson One: Building a Toggle Component

--- src/components/1-Toggle-App/index.js
+++ src/components/1-Toggle-App/index.js
@@ -1,5 +1,30 @@
 import React, { Component } from 'react';
 
-const Toggle = () => <div>Toggle</div>;
+const Switch = ({ on, className = '', ...props }) => {
+  return (
+    <div className="toggle">
+      <input className="toggle-input" type="checkbox" />
+      <button
+        className={`${className} toggle-btn ${
+          on ? 'toggle-btn-on' : 'toggle-btn-off'
+        }`}
+        aria-expanded={on}
+        {...props}
+      />
+    </div>
+  );
+};
+
+class Toggle extends Component {
+  state = { on: false };
+
+  toggle = () => {
+    this.setState(state => ({ on: !this.state.on }));
+  };
+  render() {
+    const { on } = this.state;
+    return <Switch on={on} onClick={this.toggle} />;
+  }
+}
 
 export default Toggle;
