b9c34c5a5aa69a952ff6f460b0a525fc5d142800
Raymi Saldomando <raymi@able.co>
2018-03-09 16:20:39 -0500

    React Lesson Four: Higher Order Components

--- src/components/1-Toggle-App/index.js
+++ src/components/1-Toggle-App/index.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
 import Proptypes from 'prop-types';
 
-// The problem with mapping children is that we don't have structural flexbility, meaning that the elements being rendered always have to be direct descendands of their parent otherwhise, the functionality breaks. To solve this we need to use context
+/* If we wanted to add another component inside our Toggle component tree we need to wire Context to it. But what happens if we wanted to use the Toggle component across the application? It's not very safe to wire it with Context because at the time it's an experimental API so any changes or updates on the API, might cause big trouble for the whole app. To work around this we'll create a Higher Order Component and inside that HOC we will "save" the context functionality so that we can only wire Context to a children instead to the entire Toggle Component parent
+
+Higher Order Components allows us to separate functinality that can be rehuse across different components. It doesn't matter which kind, the HOC "saves" the functionality and we just add the component that we want to enhanced the functionality to.
+*/
 
 const TOGGLE_CONTEXT = '__toggle__';
 
-const ToggleOn = ({ children }, context) => {
-  const { on } = context[TOGGLE_CONTEXT];
+const withToggle = Component => {
+  const Wrapper = (props, context) => {
+    const componentContext = context[TOGGLE_CONTEXT];
+    return <Component {...componentContext} {...props} />;
+  };
+
+  Wrapper.contextTypes = {
+    [TOGGLE_CONTEXT]: Proptypes.object.isRequired
+  };
+
+  return Wrapper;
+};
+
+const ToggleOn = withToggle(({ children, on }) => {
   return on ? children : null;
-};
+});
 
-ToggleOn.contextTypes = { [TOGGLE_CONTEXT]: Proptypes.object.isRequired };
-
-const ToggleOff = ({ children }, context) => {
-  const { on } = context[TOGGLE_CONTEXT];
+const ToggleOff = withToggle(({ children, on }) => {
   return on ? null : children;
-};
+});
 
-ToggleOff.contextTypes = { [TOGGLE_CONTEXT]: Proptypes.object.isRequired };
-
-const ToggleButton = (props, context) => {
-  const { on, toggle } = context[TOGGLE_CONTEXT];
+const ToggleButton = withToggle(({ on, toggle }, props) => {
   return <Switch on={on} onClick={toggle} {...props} />;
-};
+});
 
-ToggleButton.contextTypes = { [TOGGLE_CONTEXT]: Proptypes.object.isRequired };
+const MyOwnToggleBtn = withToggle(({ on, toggle }) => (
+  <button onClick={toggle}>{on ? 'on' : 'off'}</button>
+));
 
 class Toggle extends Component {
   static On = ToggleOn;
@@ -73,8 +84,9 @@ const ToggleWrap = () => {
   return (
     <Toggle onToggle={on => console.log('toggle', on)}>
       <Toggle.On>The button is on</Toggle.On>
-      <Toggle.Button />
+      <MyOwnToggleBtn />
       <Toggle.Off>The button is off</Toggle.Off>
+      <Toggle.Button />
     </Toggle>
   );
 };
