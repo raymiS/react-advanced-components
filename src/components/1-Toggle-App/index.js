f8ba601b6f3dda8f0c21274eccad5b1cab256249
Raymi Saldomando <raymi@able.co>
2018-03-16 12:45:38 -0500

    React Lesson Seven: Render Props React

--- src/components/1-Toggle-App/index.js
+++ src/components/1-Toggle-App/index.js
@@ -15,16 +15,46 @@ const Switch = ({ on, className = '', ...props }) => {
   );
 };
 
+// Working with render props allows us to crate a function and return it in the render method of a class component. This way we have more control over how to diplay our components and functionality with any kind of tree html depth that we want without using things like the Context API
+
 class Toggle extends Component {
   state = { on: false };
+  static defaultProps = { onToggle: () => {} };
 
   toggle = () => {
-    this.setState(state => ({ on: !this.state.on }));
+    this.setState(
+      ({ on }) => ({ on: !on }),
+      () => this.props.onToggle(this.state.on)
+    );
   };
+
   render() {
-    const { on } = this.state;
-    return <Switch on={on} onClick={this.toggle} />;
+    return this.props.render({
+      on: this.state.on,
+      toggle: this.toggle
+    });
   }
 }
 
-export default Toggle;
+function render({ on, toggle }) {
+  return <Switch on={on} onClick={toggle} />;
+}
+
+const ToggleWrap = () => {
+  return (
+    <Toggle
+      onToggle={on => console.log('toggle', on)}
+      //render={render}
+      render={({ on, toggle }) => (
+        <div>
+          <Switch on={on} onClick={toggle} />
+          <div>
+            <h1>{on ? 'on' : 'off'}</h1>
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default ToggleWrap;
